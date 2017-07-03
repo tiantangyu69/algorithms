414434e9944adbb28d4cf214a91cce4cf22823a6
lizhitao <identity69>
2017-07-03 20:11:05 +0800

    插入排序

--- src/main/java/me/flyness/algorithms/sort/InsertionSort.java
+++ src/main/java/me/flyness/algorithms/sort/InsertionSort.java
@@ -11,40 +11,24 @@ public class InsertionSort implements Sort {
 
     @Override
     public void sort(int[] array) {
-        sort(array, Direction.LEFT);
-    }
 
-    public void sort(int[] array, Direction direction) {
-        if (direction == Direction.LEFT) {
-            int j;
+        int j;
 
-            for (int i = 1; i < array.length; i++) {
-                int tmp = array[i];
+        for (int i = 1; i < array.length; i++) {
+            int tmp = array[i];
 
-                for (j = i; j > 0 && tmp < array[j - 1]; j--) {
-                    array[j] = array[j - 1];
-                }
-
-                array[j] = tmp;
+            for (j = i; j > 0 && tmp < array[j - 1]; j--) {
+                array[j] = array[j - 1];
             }
-        } else if (direction == Direction.RIGHT) {
-            int j;
 
-            for (int i = 1; i < array.length; i++) {
-                int tmp = array[i];
-
-                for (j = i; j > 0 && tmp > array[j - 1]; j--) {
-                    array[j] = array[j - 1];
-                }
-
-                array[j] = tmp;
-            }
+            array[j] = tmp;
         }
     }
 
+
     public static void main(String[] args) {
         int[] arr = Ints.INT_ARRAY;
-        new InsertionSort().sort(arr, Direction.LEFT);
+        new InsertionSort().sort(arr);
 
         Ints.printIntArray(arr);
     }
