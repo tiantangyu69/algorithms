a37aedfbc1d7e74abb97dadc521f73feb39ceeba
lizhitao <identity69>
2017-07-06 16:00:34 +0800

    希尔排序

--- src/main/java/me/flyness/algorithms/sort/impl/ShellSort.java
+++ src/main/java/me/flyness/algorithms/sort/impl/ShellSort.java
@@ -12,28 +12,21 @@ public class ShellSort extends AbstractSort {
         int step = 3;
         int size = array.length;
         int h = 1;
-        while (h < size / step) {
+        while (h < size / step)
             h = step * h + 1;
-            System.out.println("h: " + h);
-        }
 
         while (h >= 1) {
             for (int i = h; i < size; i++) {
-                for (int j = i; j >= h && array[j] < array[j - 1]; j -= h) {
+                for (int j = i; j >= h && array[j] < array[j - h]; j -= h) {
                     exchange(array, j, j - h);
                 }
             }
 
-            System.out.println("-----------------h: " + h);
             h = h / step;
-
-            return;
         }
     }
 
     public static void main(String[] args) {
-//        run(new ShellSort());
-
-        run(new ShellSort(), new int[]{25, 6, 7, 48, 5, 99, 88, 10, 16, 29});
+        run(new ShellSort());
     }
 }
